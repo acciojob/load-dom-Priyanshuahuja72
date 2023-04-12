3c6630225148406f48f013fda9172f5a9985f55b
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-04-12 13:33:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //your JS code here. If required.
 function add_element () {
-    const template = document.createElement('div');
-    template.innerHTML = "DOM load success";
-    
-    document.body.appendChild(template);
+   document.body.innerHTML = "DOM load success"
 }
 function myLoad()
 {
